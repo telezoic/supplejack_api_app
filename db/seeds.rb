d5699c9f2861e58a93492a9138d96e53ebc89ec8
Benedict Aluan <benedict.aluan@gmail.com>
2016-03-03 09:03:33 +1300

    Added cloud66 deploy hooks.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+default_user = SupplejackApi::User.find_or_create_by({name: 'SJ API', username: 'sjapi', email: 'info@boost.co.nz', role: 'admin'})
+default_user.authentication_token = 'sjapikey'
+default_user.save
